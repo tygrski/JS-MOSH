e003df9df15dbf56b91ab22d8b75fb7d6820e7ba
Travis Tybor <travis.tybor.tt@gmail.com>
2022-05-15 20:35:19 -0500

    Add files via upload

---
+++ Mosh-JS/index.js
@@ -0,0 +1,58 @@
+// browser console log
+// let name = 'Mosh';
+// console.log(name);
+
+// let firstName='Travis';
+// let lastNme="Tybor";
+// console.log(firstName);
+// console.log(lastNme);
+// ----------------------------
+// data types
+// const intrestRate = 0.3;
+
+// console.log(intrestRate);
+
+
+// let name = 'Travis'; 
+// let age = 30 
+// let isApproved = false; 
+// let firstName = undefined;
+// let selectedColor = null; 
+// =============================
+// objects
+
+// let person = {
+//     name: 'Travis',
+//     age: 40
+// };
+
+// Dot notation
+// person.name = "Mark";
+
+// bracket notation
+// let selection = 'name';
+// person['selection'] = 'Tom';
+
+// console.log(person.name);
+
+// ===================================
+// Arrays
+// let selectedColors = ['red', 'blue'];
+// selectedColors[2] = 1;
+// console.log(selectedColors.length);
+
+// =========================================
+// Functions
+function greet(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
+} 
+
+function square (number) {
+    return number * number;
+}
+
+greet('John', 'Smith');
+greet('Mary', 'Wells');
+
+// let number = square(2);
+console.log(square(2));
